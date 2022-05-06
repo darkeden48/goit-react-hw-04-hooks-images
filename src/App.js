9fc0665f942d145ad88cc8677171b74f4915c59c
darkeden48 <dzalex72@list.ru>
2022-05-07 00:01:01 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import "./App.css";
 import Searchbar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Modal from "./components/Modal/Modal";
+import Loader from "./components/Loader/Loader";
+import Button from "./components/Button/Button";
+import { fetchImage } from "./services/image-api";
 
 export default function App() {
   const [input, setInput] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [modalImage, setModalImage] = useState(null);
   const [modalAlt, setModalAlt] = useState(null);
+  const [image, setImage] = useState([]);
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(null);
+
+  useEffect(() => {
+    setImage([]);
+    setPage(1);
+  }, [input]);
+
+  useEffect(() => {
+    if (!input || input === " ") {
+      return;
+    }
+    setStatus("pending");
+    fetchImage(input, page)
+      .then((data) => {
+        if (data.hits.length === 0) {
+          setShowLoadMoreBtn(false);
+          setStatus("rejected");
+          return;
+        }
+        setImage((prevImage) =>
+          page === 1 ? data.hits : [...prevImage, ...data.hits]
+        );
+        setShowLoadMoreBtn(true);
+        setStatus("resolved");
+        if (setPage) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth",
+          });
+        }
+        if (data.hits.length < 15) {
+          setShowLoadMoreBtn(false);
+        }
+        return;
+      })
+      .catch((error) => setStatus("rejected"));
+  }, [input, page]);
 
   const handleSubmite = (input) => {
     setInput(input);
@@ -24,11 +69,19 @@ export default function App() {
     modalToggle();
   };
 
+  const onLoadMoreClick = () => {
+    setPage(page + 1);
+  };
   return (
     <div>
       <Searchbar onSubmit={handleSubmite} />
-      <ImageGallery input={input} onOpen={onOpenLargeImage} />
-
+      {status === "idle" && <p>Введите название картинки</p>}
+      {status === "resolved" && (
+        <ImageGallery onOpen={onOpenLargeImage} image={image} />
+      )}
+      {status === "pending" && <Loader />}
+      {status === "rejected" && <h1>Картинок по запросу {input} не найдено</h1>}
+      {showLoadMoreBtn && <Button onClick={onLoadMoreClick} />}
       {showModal && (
         <Modal closeModal={modalToggle}>
           <img src={modalImage} alt={modalAlt} />
@@ -37,3 +90,10 @@ export default function App() {
     </div>
   );
 }
+ImageGallery.propTypes = {
+  image: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
